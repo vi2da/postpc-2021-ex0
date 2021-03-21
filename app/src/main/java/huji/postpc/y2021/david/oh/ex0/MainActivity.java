303da7f25b8d5b2aa460253e34811bd8634ea3a7
unknown <david.ohayon@mail.huji.ac.il>
2021-03-22 01:13:21 +0200

    first commit

---
+++ app/src/main/java/huji/postpc/y2021/david/oh/ex0/MainActivity.java
@@ -0,0 +1,14 @@
+package huji.postpc.y2021.david.oh.ex0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
